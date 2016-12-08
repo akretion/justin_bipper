10f6ad5e11768b82c7b67734283afaacce64687f
hparfr <raph@hpar.fr>
2016-12-08 16:24:45 +0100

    fix chargement de carriers

--- justin/src/assemblage/carrier.page.ts
+++ justin/src/assemblage/carrier.page.ts
@@ -1,11 +1,23 @@
 import {Component} from '@angular/core';
+import {NavParams, ViewController} from 'ionic-angular';
+import {ProductsProvider} from '../models/Products.provider';
 
 @Component({
   templateUrl: 'carrier.html',
 })
 export class CarrierPage {
-  constructor(shipment)
+  carriers = [];
+  constructor(
+    public params: NavParams,
+    public view: ViewController,
+    public productsProvider: ProductsProvider
+)
   {
-      console.log('je suis carrier page constructor', shipment);
+      console.log('je suis carrier page constructor', params);
+      this.carriers = this.productsProvider.get_carriers(params['data']['shipment']);
+  }
+  close() {
+    console.log('on ferme');
+    this.view.dismiss();
   }
 }
