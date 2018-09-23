29bda2148220b52a3c86c298343a48c90211fa38
Marcin Wojtysiak <m.wojtysiak@oliverstore.com>
2018-09-23 16:31:17 +0200

    Standard product ship on button
    - still missing printing ship label

--- justin/src/standardProducts/standardProducts.picking.page.ts
+++ justin/src/standardProducts/standardProducts.picking.page.ts
@@ -7,6 +7,8 @@ import {PrintServices} from '../models/PrintServices';
 import {RouteService} from '../models/route.Service';
 import {StandardProductsProvider} from '../models/StandardProducts.provider';
 
+import {StandardProductsPage} from './standardProducts.page';
+
 @Component({
   templateUrl: 'standardProducts.picking.html',
 })
@@ -53,19 +55,46 @@ export class StandardProductsPickingPage {
   }
 
   addIt(scanned) {
+    // get index of product
     let idx: number = this.picking.move_lines.findIndex(x => x.name == scanned);
 
+    // check if scanned product is in products to pick array
     if (idx != -1) {
-      this.model.products.push(this.picking.move_lines[idx])
-      this.productsScanned++
+      // get product object
+      let product = this.picking.move_lines[idx];
+      
+      let idxModel: number = this.model.products.findIndex(x => x.name == product.name);
+      
+      if (idxModel == -1) {
+        product['product_pickied_qty'] = 1
+        this.model.products.push(product)
+      } else {
+        this.model.products[idxModel].product_pickied_qty++;
+      }
 
+      idxModel = this.model.products.findIndex(x => x.name == product.name);
+
+      if (this.model.products[idxModel].product_pickied_qty == this.model.products[idxModel].product_qty) {
+        // increment scanned products counter
+        this.productsScanned++
+      }
+
+      // check if we can procced to next step
       if (this.productsScanned == this.picking.move_lines.length){
         this.model.procced = true;
+        this.model.products.forEach(element => {
+          console.log(element)
+          if (element.product_pickied_qty != element.product_qty) {
+            this.model.procced = false;
+          }
+        });
       }
     } else {
       let newProd: any = {}
       newProd['isExpected'] = false
       newProd['name'] = scanned
+      newProd['product_pickied_qty'] = 1
+      newProd['product_qty'] = 0
       this.model.products.push(newProd)
     }
   }
@@ -82,9 +111,12 @@ export class StandardProductsPickingPage {
     this.standardProductsProvider.doPick(this.model).then(
       x => {
         console.log(x)
+        let weight = this.model.weight
         this.model = x
+        this.model.weight = weight;
         this.model.pickDone = true;
         this.model.procced = true;
+        this.doPack();
       },
       err => {
         console.log(err)
@@ -100,6 +132,7 @@ export class StandardProductsPickingPage {
         this.model = x
         this.model.packDone = true;
         this.model.procced = true;
+        this.doShip();
       },
       err => {
         console.log(err)
@@ -111,13 +144,19 @@ export class StandardProductsPickingPage {
     this.model.weight = this.model.weight;
     this.standardProductsProvider.doShip(this.model.picking_name, this.model.package[1]).then(
       x => {
-        console.log(x)
-        this.model.shipDone = true;
+        if (x) {
+          this.model.shipDone = true;
+          this.navCtrl.push(StandardProductsPage)
+        }
       },
       err => {
         console.log(err)
       }
     )
+  }
+
+  private newMethod(x: any) {
+    console.log(x);
   }
 
   reset() {
@@ -136,12 +175,39 @@ export class StandardProductsPickingPage {
 
   removeOne(product) {
     let idx = this.model.products.findIndex(x => x.name == product.name)
-    let rmProduct: any = this.model.products.splice(idx,1)
-    if (rmProduct[0].isExpected) {
-      this.productsScanned--;
-      if (this.productsScanned != this.picking.move_lines.length) {
-        this.model.shipNow = false;
+    
+    if (product.product_pickied_qty == product.product_qty && product.product_qty == 1) {
+      let rmProduct: any = this.model.products.splice(idx,1)
+      if (rmProduct[0].isExpected) {
+        this.productsScanned--;
+        this.model.procced = false;
+        if (this.productsScanned != this.picking.move_lines.length) {
+          this.model.shipNow = false;
+        }
       }
+    } else if (product.product_pickied_qty == 1) {
+      let rmProduct: any = this.model.products.splice(idx,1)
+      if (rmProduct[0].isExpected) {
+        this.productsScanned--;
+        this.model.procced = false;
+        if (this.productsScanned != this.picking.move_lines.length) {
+          this.model.shipNow = false;
+        }
+      }
+    } else {
+      this.model.products[idx].product_pickied_qty--;
     }
+
+    // check if we can procced to next step
+    if (this.productsScanned == this.picking.move_lines.length){
+      this.model.procced = true;
+      this.model.products.forEach(element => {
+        console.log(element)
+        if (element.product_pickied_qty != element.product_qty) {
+          this.model.procced = false;
+        }
+      });
+    }
+
   }
 }
