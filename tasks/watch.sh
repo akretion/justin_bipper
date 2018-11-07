304867a865ed138a57b36269c46e146d345f63cc
Marcin Wojtysiak <m.wojtysiak@oliverstore.com>
2018-11-07 09:34:49 +0100

    Release 1.2.4

--- tasks/watch.sh
+++ tasks/watch.sh
@@ -13,20 +13,20 @@ if [ "$WATCH_DO_CLEAN" = true ] ; then
     # install all node dep
     npm install
 
-    # check if original project config was modified
-    if [ -f ./ionic.config.old.json ]; then
-        # restore original project config file
-        rm -rf ./ionic.config.json
-        mv ./ionic.config.old.json ./ionic.config.json
-    fi
-
-    # move original file to old file
-    mv $OS_BUILD/src/justin/ionic.config.json $OS_BUILD/src/justin/ionic.config.old.json
-
-    # copy oliverstore config file to root of project
-    cp $OS_BUILD/etc/config/ionic.config.json $OS_BUILD/src/justin/
-
 fi
+
+# check if original project config was modified
+if [ -f ./ionic.config.old.json ]; then
+    # restore original project config file
+    rm -rf ./ionic.config.json
+    mv ./ionic.config.old.json ./ionic.config.json
+fi
+
+# move original file to old file
+mv $OS_BUILD/src/justin/ionic.config.json $OS_BUILD/src/justin/ionic.config.old.json
+
+# copy oliverstore config file to root of project
+cp $OS_BUILD/etc/config/ionic.config.json $OS_BUILD/src/justin/
 
 # watch
 ionic-app-scripts serve
