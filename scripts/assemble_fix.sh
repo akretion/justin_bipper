afe4f3e54c5e172674d0b8b727deb84e88f0830b
Marcin Wojtysiak <m.wojtysiak@oliverstore.com>
2021-01-11 09:28:12 +0100

    Updates in assemble files to respect new way of handling build libs

---
+++ scripts/assemble_fix.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# clear target directory
+/os/bin/clean.sh
+
+# assemble webapp and put in proper directory
+cd $OS_BUILD/src
+
+# decompress node libs package
+tar xf node_packages.tar.xz
+
+# add path to exec
+PATH=$PATH:./node_modules/.bin/
+
+# build the project
+npm run build
+
+# copy build project to dist directory
+cp -R www/* $OS_TARGET
