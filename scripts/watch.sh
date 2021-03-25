8927a0c6db939fa1388e0e0d04ffec69ad376454
Marcin Wojtysiak <m.wojtysiak@oliverstore.com>
2021-03-25 11:55:32 +0100

    Improve more scripts

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,32 +1,51 @@
 #!/bin/bash
 
-# set working dir
+source $OS_EXTRAS/bash/bash-utils.sh
+
+Stage "Watch"
+
+Step "Prepare the env"
+Task "Set working directory"
 cd $OS_BUILD/src
 
-# add path to exec
+Task "Set path to node bin directory"
 PATH=$PATH:./node_modules/.bin/
+Task "End prepare the env step"
 
+Step "Prepare the app" 
+Task "Check if set clean env before process with run the app"
 if [ "$WATCH_DO_CLEAN" = true ] ; then
-    # delete contnet of build watch dir
+    Task "Delete contnet of build watch dir"
     /os/bin/clean.sh
 
-    # install all node dep
-    npm install
-
+    Task "Check if exist compressed file with build libs, if yes decompress it, if not donwload build libs"
+    if [ -f ./node_packages.tar.xz ]; then
+        Task "Decompress build libs"
+        tar xf node_packages.tar.xz
+    else
+        Task "Install build libs "
+        npm install
+    fi
 fi
+Task "End prepare the app step"
 
-# check if original project config was modified
+
+Step "Configure the local app server" 
+
+# this task is only to be sure that we don't loose original config file
+Task "Check if original project config was modified"
 if [ -f ./ionic.config.old.json ]; then
-    # restore original project config file
+    Task "Restore original project config file"
     rm -rf ./ionic.config.json
     mv ./ionic.config.old.json ./ionic.config.json
 fi
 
-# move original file to old file
+Task "Move original file to old file"
 mv $OS_BUILD/src/ionic.config.json $OS_BUILD/src/ionic.config.old.json
 
-# copy oliverstore config file to root of project
+Task " Copy oliverstore config file to root of project"
 cp $OS_BUILD/etc/config/bipper/ionic.config.json $OS_BUILD/src/
+Task "End configure the local app server step"
 
-# watch
+Step "Watch"
 ionic-app-scripts serve
