b45f7211b76ded7e815f7e1f596a004209482298
Marcin Wojtysiak <m.wojtysiak@oliverstore.com>
2021-03-29 14:26:24 +0200

    tech

--- tasks/assemble.sh
+++ tasks/assemble.sh
@@ -18,8 +18,6 @@ source extras/bash/bash-utils.sh
 Stage "Assemble"
 
 Step "Build the docker image"
-docker-compose --version
-docker-compose -p ${DEV_PROJECT} -f ${GPS_PROJECT_DIR}/etc/docker/docker-compose.assemble.yml config
 docker-compose -p ${DEV_PROJECT} -f ${GPS_PROJECT_DIR}/etc/docker/docker-compose.assemble.yml rm -f assembler
 docker-compose -p ${DEV_PROJECT} -f ${GPS_PROJECT_DIR}/etc/docker/docker-compose.assemble.yml up --build --force-recreate --env-file ./.env assembler
 
