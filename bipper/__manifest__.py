c7c49d267b1a3f0c80ea4afb2ee0c2179fc08242
hparfr <raphael.reverdy@akretion.com>
2022-12-20 10:02:38 +0100

    add bipper

---
+++ bipper/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Bipper",
+    "version": "14.0.0.0.1",
+    "author": "Akretion",
+    "website": "www.akretion.com",
+    "license": "AGPL-3",
+    "category": "Generic Modules",
+    "description": "Justin Bipper application (js)",
+    "depends": [
+        "web",
+        # actually depend on custom module to work but we prefer to avoid having a
+        # direct dependency on a custom module
+        "base",
+    ],
+    "installable": True,
+    "application": True,
+}
